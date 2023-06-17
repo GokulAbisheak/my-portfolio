dbefe3f10e22b5992c38dea030870e5821519f57
IT21375132 <gokulabisheak12@gmail.com>
2023-06-17 16:52:41 +0530

    Improved Responsiveness of Header

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,9 +1,12 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
 
     const [bgColor, setBgColor] = useState('');
     const [textColor, setTextColor] = useState('text-white');
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
 
     useEffect(() => {
         const handleScroll = () => {
@@ -22,6 +25,10 @@ const Header = () => {
         };
     }, []);
 
+    const handleToggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
+
     return (
         <>
             <div id='header' className={`fixed flex justify-between items-center ${bgColor} top-0 left-0 w-full h-16 z-50 px-10 max-lg:bg-white max-lg:dark:bg-gray-900`}>
@@ -29,13 +36,31 @@ const Header = () => {
                     Gokul Abisheak
                 </div>
                 <div className={`hidden lg:flex text-base space-x-8 ${textColor}`}>
-                    <a href='#home'>Home</a>
-                    <a href='#about'>About</a>
-                    <a href='#skills'>Skills</a>
-                    <a href='#project'>Project</a>
-                    <a href='#contact'>Contact</a>
+                    <a href='#home' className='hover:text-gray-700'>Home</a>
+                    <a href='#about' className='hover:text-gray-700'>About</a>
+                    <a href='#skills' className='hover:text-gray-700'>Skills</a>
+                    <a href='#project' className='hover:text-gray-700'>Project</a>
+                    <a href='#contact' className='hover:text-gray-700'>Contact</a>
+
+                </div>
+                <div className='flex lg:hidden text-gray-900 dark:text-white hover:text-primary dark:hover:text-secondary text-2xl'>
+                    <button onClick={handleToggleMobileMenu}>
+                        <FontAwesomeIcon icon={faBars} />
+                    </button>
                 </div>
             </div>
+            {isMobileMenuOpen && (
+                <div className='bg-neutral-200 dark:bg-gray-800 text-black dark:text-white lg:hidden fixed top-16 left-0 w-full z-50'>
+                    <div className='flex flex-col items-center'>
+                        <a className='block py-3 hover:text-primary dark:hover:text-secondary' href='#home'>Home</a>
+                        <a className='block py-3 hover:text-primary dark:hover:text-secondary' href='#about'>About</a>
+                        <a className='block py-3 hover:text-primary dark:hover:text-secondary' href='#skills'>Skills</a>
+                        <a className='block py-3 hover:text-primary dark:hover:text-secondary' href='#project'>Project</a>
+                        <a className='block py-3 hover:text-primary dark:hover:text-secondary' href='#contact'>Contact</a>
+                    </div>
+                </div>
+
+            )}
         </>
     );
 }
