51433c437e68b3b52b8c20c3e3f7b703b263ba7c
IT21375132 <gokulabisheak12@gmail.com>
2023-06-18 16:20:56 +0530

    Completed Volunteering Page

--- client/src/views/About/Volunteering/index.jsx
+++ client/src/views/About/Volunteering/index.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import VolunteeringData from '../../../data/VolunteeringData';
 
 const Volunteering = () => {
     return (
         <>
-        
+            <div id='volunteering' className='flex flex-col items-center min-h-screen bg-neutral-50 dark:bg-gray-900 pt-20'>
+                <h1 className='text-4xl lexend-bold text-primary dark:text-secondary mb-8'>
+                    Volunteering
+                </h1>
+                <div className='flex flex-col bg-neutral-50 dark:bg-gray-800 text-neutral-950 dark:text-neutral-50 border-2 border-b-0 border-primary dark:border-secondary new-shadow-xl mx-4'>
+                    {VolunteeringData.map((list) => (
+                        <div className='flex flex-row p-2 border-b-2 border-primary dark:border-secondary' style={{maxWidth: '500px'}}>
+                            <div className='mr-4 flex justify-center p-2' style={{width: '80px', height: '80px'}}>
+                                <img src={list.image} className='object-contain' />
+                            </div>
+                            <div className='grow pr-4'>
+                                <div>
+                                    {list.position}
+                                </div>
+                                <div className='lexend-light'>
+                                    {list.club}
+                                </div>
+                                <div className='lexend-light'>
+                                    {list.duration}
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </>
     );
 }
